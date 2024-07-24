ab333e356bf1df9dfdea3f244a686c3b7dbbb3c9
IMANARIYO <imanariyobaptiste@gmail.com>
2024-07-24 15:01:20 +0200

    adding the footer edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter} from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import NavBar from "./components/NavBar";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
+    <div className="app">
     <BrowserRouter>
       <NavBar />
+      <Footer title="The Footers" />
     </BrowserRouter>
+    </div>
   );
 };
 
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+import React from "react";
+
+function Footer({ title }) {
+  return (<>
+  <div>
+
+    hello
+  </div>
+    <footer className="footer">
+      <h1 className="footer-title">{title}</h1>
+    </footer>
+  </>
+  );
+}
+
+export default Footer;
